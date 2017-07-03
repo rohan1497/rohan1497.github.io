127f26089b263a1d1ee55fa9b4e502a4524c4d5a
avaidsr <avaids.r@gmail.com>
2017-07-03 21:58:14 +0530

    a

--- js/init.js
+++ js/init.js
@@ -5,6 +5,17 @@
     $('.parallax').parallax();
     // Initialize collapsible (uncomment the line below if you use the dropdown variation)
   //$('.collapsible').collapsible();
+  $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      //constrainWidth: false, // Does not change width of dropdown to that of the activator
+      //hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: false, // Displays dropdown below the button
+      //alignment: 'left', // Displays dropdown with edge aligned to the left of button
+      stopPropagation: false // Stops event propagation
+    }
+  );
   
   }); // end of document ready
 })(jQuery); // end of jQuery name space
