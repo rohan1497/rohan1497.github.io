5126196c126c0fdbefc5e755054a42d4bb378b31
avaidsr <avaids.r@gmail.com>
2017-07-03 20:36:36 +0530

    aa

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  //$('.collapsible').collapsible();
+  
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
